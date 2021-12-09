c4e61e7389ff829ed50cd747471dd5387287bf63
Jonas Trollvik <jonas@weightpoint.se>
2021-12-09 02:20:52 +0100

    Cleanup #2

--- src/main/kotlin/day8.kt
+++ src/main/kotlin/day8.kt
@@ -1,10 +1,10 @@
 import java.io.File
 
-private fun getInput(): List<Pair<List<String>, List<String>>> {
+private fun getInput(): List<Pair<List<Set<Char>>, List<Set<Char>>>> {
     return File("/Users/jonas/Documents/workspace/advent-of-code-2021/src/main/kotlin/day8input.txt").readText()
         .split("\n").map {
             it.split(" | ")
-        }.map { Pair(it[0].split(" ").map { it.toCharArray().sorted().joinToString("") }, it[1].split(" ").map { it.toCharArray().sorted().joinToString("") }) }
+        }.map { Pair(it[0].split(" ").map { it.toCharArray().toSet() }, it[1].split(" ").map { it.toCharArray().toSet() }) }
 }
 
 fun run8pt1(): Int {
@@ -12,7 +12,7 @@ fun run8pt1(): Int {
     val charsToNum = mapOf(2 to 1, 4 to 4, 3 to 7, 7 to 8)
 
     return input.sumBy { it.second.mapNotNull {
-        charsToNum[it.length]
+        charsToNum[it.size]
     }.size }
 }
 
@@ -24,33 +24,31 @@ fun run8pt2(): Int {
         row.second.map { charsToNum[it].toString() }.joinToString("").toInt()
     }
 }
-fun String.toList() = toCharArray().toList()
-fun String.containsAll(str: String?) = toList().containsAll(str!!.toList())
-fun mapCharsToNum(signals: List<String>): Map<String, Int> {
+fun mapCharsToNum(signals: List<Set<Char>>): Map<Set<Char>, Int> {
     val knownCharsToNumByLength = mapOf(2 to 1, 4 to 4, 3 to 7, 7 to 8)
-    val known = signals.filter { knownCharsToNumByLength[it.length] != null }.associateBy({ knownCharsToNumByLength[it.length]!! }, {it}).toMutableMap()
+    val known = signals.filter { knownCharsToNumByLength[it.size] != null }.associateBy({ knownCharsToNumByLength[it.size]!! }, {it}).toMutableMap()
     val remainingSignals = signals.toMutableList().filterNot {known.values.contains(it) }
-    known[2] = remainingSignals.filter { it.length == 5 }.filter {
-        if (it.containsAll(known[1])) {
+    known[2] = remainingSignals.filter { it.size == 5 }.filter {
+        if (it.containsAll(known[1]!!)) {
             known[3] = it
             false
         } else true
     }.filter {
-        if (it.toList().containsAll(known[4]!!.toList().filterNot { known[1]!!.toList().contains(it) })) {
+        if (it.containsAll(known[4]!!.filterNot { known[1]!!.contains(it) })) {
             known[5] = it
             false
         } else true
     }.first()
-    known[9] = remainingSignals.filter { it.length == 6 }.filter {
-        if (!it.containsAll(known[7])) {
+    known[9] = remainingSignals.filter { it.size == 6 }.filter {
+        if (!it.containsAll(known[7]!!)) {
             known[6] = it
             false
         } else true
     }.filter {
-        if (!it.containsAll(known[4])) {
+        if (!it.containsAll(known[4]!!)) {
             known[0] = it
             false
         } else true
     }.first()
-    return known.entries.associateBy({it.value}, {it.key!!})
+    return known.entries.associateBy({it.value}, {it.key})
 }
