3ecf635264cfea18950f8d9af9f6303d23da9fe7
Jonas Trollvik <jonas@weightpoint.se>
2021-12-09 03:05:24 +0100

    Cleanup last

--- src/main/kotlin/day8.kt
+++ src/main/kotlin/day8.kt
@@ -24,33 +24,26 @@ fun run8pt2(): Int {
         row.second.map { charsToNum[it].toString() }.joinToString("").toInt()
     }
 }
-enum class Op {
-    INC {
-        override fun op(str: Set<Char>, o: Collection<Char>?) = str.containsAll(o!!)
-    }, EXC {
-        override fun op(str: Set<Char>, o: Collection<Char>?) = !str.containsAll(o!!)
-    };
-    abstract fun op(str: Set<Char>, o: Collection<Char>?): Boolean
-}
-data class Row(val str: Collection<Char>?, val op: Op)
+val inc = fun (str: Set<Char>, o: Collection<Char>?) = str.containsAll(o!!);
+val exl = fun (str: Set<Char>, o: Collection<Char>?) = !str.containsAll(o!!)
+
 fun mapCharsToNum(signals: List<Set<Char>>): Map<Set<Char>, Int> {
     val knownCharsToNumByLength = mapOf(2 to 1, 4 to 4, 3 to 7, 7 to 8)
     val known = signals.filter { knownCharsToNumByLength[it.size] != null }.associateBy({ knownCharsToNumByLength[it.size]!! }, {it}).toMutableMap()
     val remainingSignals = signals.filterNot {known.values.contains(it) }.sortedBy { it.size }.toMutableList()
-    for ((match, number) in listOf(
-        Row(known[1], Op.INC) to 3,
-        Row(known[4]!!.filterNot { known[1]!!.contains(it) }, Op.INC) to 5,
-        null to 2,
-        Row(known[7], Op.EXC) to 6,
-        Row(known[4], Op.EXC) to 0,
-        null to 9
+    for ((match, fn, number) in listOf(
+        Triple(known[1], inc, 3),
+        Triple(known[4]!!.filterNot { known[1]!!.contains(it) }, inc, 5),
+        Triple(null, inc, 2),
+        Triple(known[7], exl, 6),
+        Triple(known[4], exl, 0),
+        Triple(null,exl, 9)
     )) {
         known[number] = if (match != null) {
-            remainingSignals.removeAt(remainingSignals.indexOfFirst { match.op.op(it, match.str)})
+            remainingSignals.removeAt(remainingSignals.indexOfFirst { fn(it, match)})
         } else {
             remainingSignals.removeFirst()
         }
-
     }
     return known.entries.associateBy({it.value}, {it.key})
 }
