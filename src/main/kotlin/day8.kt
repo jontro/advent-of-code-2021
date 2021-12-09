0ad654be224d660a1fe8a9c6beb6d8e5dc601856
Jonas Trollvik <jonas@weightpoint.se>
2021-12-09 02:53:54 +0100

    "Cleanup"

--- src/main/kotlin/day8.kt
+++ src/main/kotlin/day8.kt
@@ -24,31 +24,33 @@ fun run8pt2(): Int {
         row.second.map { charsToNum[it].toString() }.joinToString("").toInt()
     }
 }
+enum class Op {
+    INC {
+        override fun op(str: Set<Char>, o: Collection<Char>?) = str.containsAll(o!!)
+    }, EXC {
+        override fun op(str: Set<Char>, o: Collection<Char>?) = !str.containsAll(o!!)
+    };
+    abstract fun op(str: Set<Char>, o: Collection<Char>?): Boolean
+}
+data class Row(val str: Collection<Char>?, val op: Op)
 fun mapCharsToNum(signals: List<Set<Char>>): Map<Set<Char>, Int> {
     val knownCharsToNumByLength = mapOf(2 to 1, 4 to 4, 3 to 7, 7 to 8)
     val known = signals.filter { knownCharsToNumByLength[it.size] != null }.associateBy({ knownCharsToNumByLength[it.size]!! }, {it}).toMutableMap()
-    val remainingSignals = signals.toMutableList().filterNot {known.values.contains(it) }
-    known[2] = remainingSignals.filter { it.size == 5 }.filter {
-        if (it.containsAll(known[1]!!)) {
-            known[3] = it
-            false
-        } else true
-    }.filter {
-        if (it.containsAll(known[4]!!.filterNot { known[1]!!.contains(it) })) {
-            known[5] = it
-            false
-        } else true
-    }.first()
-    known[9] = remainingSignals.filter { it.size == 6 }.filter {
-        if (!it.containsAll(known[7]!!)) {
-            known[6] = it
-            false
-        } else true
-    }.filter {
-        if (!it.containsAll(known[4]!!)) {
-            known[0] = it
-            false
-        } else true
-    }.first()
+    val remainingSignals = signals.filterNot {known.values.contains(it) }.sortedBy { it.size }.toMutableList()
+    for ((match, number) in listOf(
+        Row(known[1], Op.INC) to 3,
+        Row(known[4]!!.filterNot { known[1]!!.contains(it) }, Op.INC) to 5,
+        null to 2,
+        Row(known[7], Op.EXC) to 6,
+        Row(known[4], Op.EXC) to 0,
+        null to 9
+    )) {
+        known[number] = if (match != null) {
+            remainingSignals.removeAt(remainingSignals.indexOfFirst { match.op.op(it, match.str)})
+        } else {
+            remainingSignals.removeFirst()
+        }
+
+    }
     return known.entries.associateBy({it.value}, {it.key})
 }
