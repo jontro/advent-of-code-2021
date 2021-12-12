6ce71a89702dae2ec8951f1c6d94a5a0205cf491
Jonas Trollvik <jonas@weightpoint.se>
2021-12-12 23:34:01 +0100

    Cleanup day 11

--- src/main/kotlin/day11.kt
+++ src/main/kotlin/day11.kt
@@ -7,7 +7,7 @@ private fun getInput(): MutableList<MutableList<Int>>{
             it.toCharArray().map { it.toString().toInt() }.toMutableList()
         }.toMutableList()
 }
-fun flash(input: MutableList<MutableList<Int>>, x:Int ,y: Int) {
+private fun flash(input: MutableList<MutableList<Int>>, x:Int ,y: Int) {
     for ((newX, newY) in listOf(
         -1 to -1, 0 to -1, 1 to -1,
         -1 to  0,          1 to  0,
@@ -22,26 +22,25 @@ fun flash(input: MutableList<MutableList<Int>>, x:Int ,y: Int) {
         }catch (t: IndexOutOfBoundsException) {}
     }
 }
+
+
+private fun doFlashRun(_input: MutableList<MutableList<Int>>): MutableList<MutableList<Int>> {
+    var input = _input
+    input = input.map { it.map { it + 1 }.toMutableList() }.toMutableList()
+    input.withIndex().map { (y, row) -> row.withIndex().filter { (_, v) -> v == 10 }.map { (x, _) -> x to y } }
+        .flatten().forEach { (x, y) -> flash(input, x, y) }
+    input = input.map { it.map { if (it > 9) 0 else it }.toMutableList() }.toMutableList()
+    return input
+}
+
 fun run11pt1(): Int {
     var input = getInput()
     var totalSum = 0
 
     for (i in 0 .. 99) {
-        input = input.map { it.map { it + 1 }.toMutableList() }.toMutableList()
-
-        val clonedInput = input.map { it.map { it }.toMutableList() }.toMutableList()
-        for ((y, row) in clonedInput.withIndex()) {
-            for ((x, v) in row.withIndex()) {
-                if (v == 10) {
-                    flash(input, x, y)
-                }
-            }
-        }
-        totalSum += input.sumBy { it.count { it > 9 } }
-
-        input = input.map { it.map { if (it > 9) 0 else it }.toMutableList() }.toMutableList()
+        input = doFlashRun(input)
+        totalSum += input.sumBy { it.count { it == 0 } }
     }
-
     return totalSum
 }
 
@@ -49,18 +48,9 @@ fun run11pt2(): Int {
     var input = getInput()
 
     for (i in 0 .. 9999) {
-        input = input.map { it.map { it + 1 }.toMutableList() }.toMutableList()
-        val clonedInput = input.map { it.map { it }.toMutableList() }.toMutableList()
-        for ((y, row) in clonedInput.withIndex()) {
-            for ((x, v) in row.withIndex()) {
-                if (v == 10) {
-                    flash(input, x, y)
-                }
-            }
-        }
-        if (input.sumBy { it.count { it > 9 } } == input.size * input[1].size)
+        input = doFlashRun(input)
+        if (input.sumBy { it.count { it == 0 } } == input.size * input[1].size)
             return i + 1
-        input = input.map { it.map { if (it > 9) 0 else it }.toMutableList() }.toMutableList()
     }
 
     return 0
